910326b31e7e42f060ae717b74bb85530d1ab8f5
Dorian Niemiec <dorian@ferron.sh>
2026-04-18 13:17:45 +0200

    style: collapse one match arm with nested if

--- cegla/src/client/response.rs
+++ cegla/src/client/response.rs
@@ -37,11 +37,9 @@ where
     let mut is_status_header = false;
 
     match &header.name.to_lowercase() as &str {
-      "location" => {
-        if !(300..=399).contains(&status_code) {
-          // Implicitly set status code to 302 if Location header is present
-          status_code = 302;
-        }
+      "location" if !(300..=399).contains(&status_code) => {
+        // Implicitly set status code to 302 if Location header is present
+        status_code = 302;
       }
       "status" => {
         is_status_header = true;
